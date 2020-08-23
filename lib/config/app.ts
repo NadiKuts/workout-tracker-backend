91d167f6a3202ab9622f0843c6b722834532b033
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2020-08-23 17:52:49 +0200

    lib: add test-routes

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -1,10 +1,15 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+
+import { TestRoutes } from "../routes/test-routes";
 class App {
    public app: express.Application;
+   private test_routes: TestRoutes = new TestRoutes();
+
    constructor() {
       this.app = express();
       this.config();
+      this.test_routes.route(this.app);
    }
 private config(): void {
       // support application/json type post data
